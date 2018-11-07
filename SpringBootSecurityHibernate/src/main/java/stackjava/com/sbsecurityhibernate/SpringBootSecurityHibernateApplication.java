89dda1f5ad14aa11b2a7c1dd41adc94d2d93803c
stonecorter <33486327+stonecorter@users.noreply.github.com>
2018-11-07 09:58:28 +0700

    Add files via upload

---
+++ SpringBootSecurityHibernate/src/main/java/stackjava/com/sbsecurityhibernate/SpringBootSecurityHibernateApplication.java
@@ -0,0 +1,18 @@
+package stackjava.com.sbsecurityhibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class,
+	    DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
+public class SpringBootSecurityHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityHibernateApplication.class, args);
+	}
+}
